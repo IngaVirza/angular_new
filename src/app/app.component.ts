793a626f5891b1b7906bc86b98406e8675af8691
unknown <inha.virza@gmail.com>
2024-07-12 00:07:41 +0545

    login page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,6 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProfileCardComponent } from './common-ui/profile-card/profile-card.component';
-import { ProfileService } from './data/services/profile.service';
-import { Profile } from './data/intrfaces/profile.interface';
 
 @Component({
   selector: 'app-root',
@@ -11,13 +9,4 @@ import { Profile } from './data/intrfaces/profile.interface';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  profileService: ProfileService = inject(ProfileService);
-  profiles: Profile[] = [];
-
-  constructor() {
-    this.profileService.getTestAccounts().subscribe((val) => {
-      this.profiles = val;
-    });
-  }
-}
+export class AppComponent {}
